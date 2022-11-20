fd537fec953765ab5436f70d42e0986d8f8faf2b
kai staud <kai-staud@web.de>
2022-11-20 16:26:29 +0100

    Job dispatching done

--- src/job_dispatcher.rs
+++ src/job_dispatcher.rs
@@ -66,12 +66,6 @@ struct RingManager {
     r3: AllocRingBuffer<(Vector3<i32>, Vector3<i32>)>,
     read_index: [i32; 3],
     write_index: [i32; 3],
-    r1_ri: i32,
-    r1_wi: i32,
-    r2_ri: i32,
-    r2_wi: i32,
-    r3_ri: i32,
-    r3_wi: i32,
 }
 impl RingManager {
     fn new() -> RingManager {
@@ -85,12 +79,6 @@ impl RingManager {
             r1: (tr1),
             r2: (tr2),
             r3: (tr3),
-            r1_ri: (0),
-            r1_wi: (0),
-            r2_ri: (0),
-            r2_wi: (0),
-            r3_ri: (0),
-            r3_wi: (0),
             read_index: (ri_array),
             write_index: (wi_array),
         }
@@ -154,10 +142,10 @@ impl RingManager {
 
         // Is there enough space in Ring 2 to save another Vector when unpacked?
         let mut remaining_space = self.get_remaining_slots(1);
-        let mut read_pointer = 0;
         let open_requests = self.get_open_requests(0);
         let mut transferred_requests = 0;
 
+        let mut read_pointer = self.translate_read_pointer(self.read_index[0]);
         // Exit when theres enough storage space, but no pending requests
         // Also exit when not enough storage space is available
         while remaining_space > 0 && transferred_requests < open_requests {
@@ -165,10 +153,11 @@ impl RingManager {
             if remaining_space >= requested_space.try_into().unwrap() {
                 //println!("Theres enough space {}/{} left!",requested_space,remaining_space);
                 // Unpack vector from ring1 into ring2
-                read_pointer -= 1;
+                read_pointer = self.translate_read_pointer(self.read_index[0]);
                 let v = self.r1.get(read_pointer).cloned().unwrap().to_owned(); // Index needs to be r1_ri!
 
                 let cv = v.clone();
+                //println!("{}->{:?}",read_pointer,cv);
                 // Are we overwriting unused data?
                 let dv = self.get_valid_buffer_ranger(
                     1,
@@ -201,46 +190,42 @@ impl RingManager {
     fn refill_ring3(&mut self) -> i32 {
         let mut transferred_elements = 0;
         let remaining_space = self.get_remaining_slots(2);
-        let read_pointer = 0;
+        let mut read_pointer = self.translate_read_pointer(self.read_index[1]);
         let open_requests = self.get_open_requests(1);
         let mut ix = -1;
         // While r3 has empty slots:
         while remaining_space > 0 && open_requests > transferred_elements {
             println!("{}/{}", transferred_elements, open_requests);
-            let v = self.r2.get(ix.try_into().unwrap()).unwrap().clone(); // Index needs to be r1_ri!
-            println!("Reading R1 #{}:{:?}", ix, v);
-            self.update_pointer(false, 0);
+            let v = self.r2.get(read_pointer).unwrap().clone(); // Index needs to be r1_ri!
+            println!("Reading R1 #{}:{:?}", read_pointer, v);
+            self.r3.push((v.vector, v.rotation));
+            self.update_pointer(true, 1);
             transferred_elements += 1;
-            ix -= 1;
+            read_pointer = self.translate_read_pointer(self.read_index[1]); //-=1;
         }
-        /*
-                for i in 0..2 {
-                    // Get from r2:
-                    let v = self.r2.get(ix.try_into().unwrap()).unwrap().clone(); // Index needs to be r1_ri!
 
-                    if self.r3_wi >= self.r3_ri && self.r3_ri <= RING3_SIZE {
-                        // Save to r3:
-                        self.r3.push((v.vector, v.rotation));
-                        self.update_pointer(false, 0);
-                        self.r3_wi += 1;
-
-                        transferred_elements += 1;
-                        println!("Got from R3:{:?},{:?}", ix, self.r3.get(-1));
-                        ix -= 1;
-                    }
-                }
-        */
         transferred_elements
     }
 
-    fn set_job_from_ring3() {}
+    fn set_job_from_ring3(&mut self) -> Option<(Vector3<i32>, Vector3<i32>)> {
+        if self.read_index[2] < -10 {
+            self.read_index[2] = -1;
+        } else {
+            self.read_index[2] -= 1;
+        }
+        self.r3.get(self.read_index[2].try_into().unwrap()).cloned()
+    }
+
+    fn auto_refill_rings(&mut self) -> Result<(), DataRingError> {
+        _ = self.refill_ring2();
+        _ = self.refill_ring3();
+        Ok(())
+    }
 
     fn push_to_ring1(&mut self, i: Vec<TimedCoordinates>) -> i32 {
         self.r1.push(i);
-        self.r1_wi += 1;
         self.write_index[0] += 1;
-        let remaining_slot = self.r1.peek().unwrap();
-        RING1_SIZE - self.r1_wi
+        RING1_SIZE - self.write_index[0] // Todo: Should return get available space!
     }
 }
 
@@ -368,7 +353,6 @@ fn test_fill_r2() {
             rotation: Vector3::from_vec(Vec::from([4, 5, 6])),
         }
     );
-    assert_eq!(2, 2); // 2 TC transferred
 }
 
 #[test]
@@ -388,24 +372,23 @@ fn test_fill_r3() {
     };
     _ = rm.push_to_ring1(vec![tc, tc2]);
     let _ = rm.refill_ring2();
-    let _ = rm.r2.get(-2).unwrap().clone();
-
     let _ = rm.refill_ring3();
 
-    let mut r3_content = rm.r3.get(-1).unwrap().clone();
+    let r3_content = rm.set_job_from_ring3().unwrap().clone();
+    let r32_content = rm.set_job_from_ring3().unwrap().clone();
+
+    assert_eq!(
+        r32_content,
+        (
+            Vector3::from_vec(Vec::from([44, 55, 66])),
+            Vector3::from_vec(Vec::from([4, 5, 6]))
+        )
+    );
     assert_eq!(
         r3_content,
         (
             Vector3::from_vec(Vec::from([11, 22, 33])),
             Vector3::from_vec(Vec::from([1, 2, 3]))
-        )
-    );
-    r3_content = rm.r3.get(-2).unwrap().clone();
-    assert_eq!(
-        r3_content,
-        (
-            Vector3::from_vec(Vec::from([44, 55, 66])),
-            Vector3::from_vec(Vec::from([4, 5, 6]))
         )
     );
 }
@@ -426,10 +409,52 @@ fn trickle_down_r1_to_queue() {
         rotation: Vector3::from_vec(Vec::from([4, 5, 6])),
     };
     _ = rm.push_to_ring1(vec![tc, tc2]);
+
+    _ = rm.auto_refill_rings();
+
+    let r3_content = rm.set_job_from_ring3().unwrap().clone();
+    let r32_content = rm.set_job_from_ring3().unwrap().clone();
+
+    assert_eq!(
+        r32_content,
+        (
+            Vector3::from_vec(Vec::from([44, 55, 66])),
+            Vector3::from_vec(Vec::from([4, 5, 6]))
+        )
+    );
+    assert_eq!(
+        r3_content,
+        (
+            Vector3::from_vec(Vec::from([11, 22, 33])),
+            Vector3::from_vec(Vec::from([1, 2, 3]))
+        )
+    );
 }
 
 #[test]
-fn test_overpush_ring() {}
+fn test_overpush_ring() {
+    let mut rm = RingManager::new();
+    let tc = TimedCoordinates {
+        name: ("Test".to_string()),
+        timestamp: (10),
+        vector: Vector3::from_vec(Vec::from([11, 22, 33])),
+        rotation: Vector3::from_vec(Vec::from([1, 2, 3])),
+    };
+    let tc2 = TimedCoordinates {
+        name: ("Test2".to_string()),
+        timestamp: (10),
+        vector: Vector3::from_vec(Vec::from([44, 55, 66])),
+        rotation: Vector3::from_vec(Vec::from([4, 5, 6])),
+    };
+    let v = vec![tc, tc2];
+    // Fill ring1 with 10 elements!
+    for i in 0..9 {
+        let x = v.clone();
+        _ = rm.push_to_ring1(x);
+    }
+    // Eleventh element will overwrite, resulting in return value = 0
+    assert_eq!(rm.push_to_ring1(v.clone()), 0);
+}
 
 #[test]
 fn test_overpull_ring() {}
